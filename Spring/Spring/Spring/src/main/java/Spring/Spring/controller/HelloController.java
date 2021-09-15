78b7b9d87221d9b1b8c90d4cd65addafed8b9b2f
yjh-1008 <71313511+yjh-1008@users.noreply.github.com>
2021-09-15 10:15:04 +0900

    chatpter 2-3

--- Spring/Spring/Spring/src/main/java/Spring/Spring/controller/HelloController.java
+++ Spring/Spring/Spring/src/main/java/Spring/Spring/controller/HelloController.java
@@ -4,6 +4,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 
 @Controller
 public class HelloController {
@@ -18,5 +20,32 @@ public class HelloController {
         model.addAttribute("name",name);
         //hello-template.html에 Model채로 넘겨줌 
         return "hello-template";
+    }
+    @GetMapping("hello-string")
+    @ResponseBody
+    //http body구역에 직접 넣어준다. 경로가 없다. 그렇기 때문에 웹 브라우저 상에서는 hello'+name'만 나오게 된다.
+    public String helloSpring(@RequestParam("name")String name){
+        return "hello"+name;
+    }
+
+    //JSON 방식
+    @GetMapping("hello-api")
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){
+        Hello hello = new Hello();
+        hello.setName(name);
+        return hello;
+
+    }
+    static class Hello {
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
     }
 }
